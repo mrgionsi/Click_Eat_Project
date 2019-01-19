141f05a162004c29c5186aa56fa61109c55f0262
mrgionsi <pasquariello.giovanni@ericsson.com>
2019-01-19 16:17:34 +0100

    proviamo a fare il merge

--- src/connessione/ConnectionPool.java
+++ src/connessione/ConnectionPool.java
@@ -6,103 +6,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ConnectionPool {
-<<<<<<< HEAD
-  private static List<Connection> freeDbConnections;
 
-  static {
-    freeDbConnections = new LinkedList<Connection>();
-    try {
-      Class.forName("com.mysql.jdbc.Driver");
-    } catch (ClassNotFoundException e) {
-      System.out.println("DB driver not found:"+ e.getMessage());
-    } 
-  }
-  
-  private static synchronized Connection createDBConnection() throws SQLException {
-    
-    int lport=3306;
-    String rhost="172.31.44.86";
-    String host="18.191.83.131";
-    int rport=3306;
-    String user="ec2-user";
-    String password="Gionsi12345.";
-    String dbuserName = "lucaamoriello";
-    String dbpassword = "lucapass";
-    String url ="jdbc:mysql://localhost:"+lport+"/clickeat";
-        String driverName="com.mysql.jdbc.Driver";
-        Connection conn = null;
-        Session session = null;
-        
-        try {
-        java.util.Properties config = new java.util.Properties(); 
-        config.put("StrictHostKeyChecking", "no");
-        JSch jsch = new JSch();
-        session=jsch.getSession(user, host, 22);
-        session.setPassword(password);
-        session.setConfig(config);
-        session.connect();
-        System.out.println("Connected");
-        int assinged_port=session.setPortForwardingL(lport, rhost, rport);
-          System.out.println("localhost:"+assinged_port+" -> "+rhost+":"+rport);
-        System.out.println("Port Forwarded");
-        
-        //mysql database connectivity
-            Class.forName(driverName).getDeclaredConstructor().newInstance();
-            conn = DriverManager.getConnection (url, dbuserName, dbpassword);
-            
-            conn.setAutoCommit(true);
-            
-            System.out.println ("Database connection established");
-            System.out.println("DONE");
-        return conn;
-
-      }catch(Exception e){
-        e.printStackTrace();
-      }finally{
-        if(conn != null && !conn.isClosed()){
-          //System.out.println("Closing Database Connection");
-          //conn.close();
-        }
-        if(session !=null && session.isConnected()){
-          //System.out.println("Closing SSH Connection");
-          //session.disconnect();
-        }
-      }
-    return conn;
-  }
-
-
-=======
-	
 	private static List<Connection> freeConn;
 	
 	static {
 		freeConn = new LinkedList<Connection>();
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
->>>>>>> 1501595907d3bf1234d5dc3e7862ba72960801cc
 
-;		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			System.out.println("Driver not found:" + e.getMessage());
 		}
 	}
 
 	private static synchronized Connection createDBConnection() throws SQLException {
 		Connection newConnection = null;
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		String username = "focusdb";
-//		String password = "newpassword2018";
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
-//		please, if you are in this section avoid to access into my database.
+
 		String username = "clickeat";
 		String password = "clickeat";
 		
