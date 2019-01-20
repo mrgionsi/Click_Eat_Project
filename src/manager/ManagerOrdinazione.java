f19c810ffad1b1e2e8f01b27ee4f0a406e931186
Wakeed <acupito2@gmail.com>
2019-01-20 21:55:22 +0100

    modifica in ottieniOrdinazione

--- src/manager/ManagerOrdinazione.java
+++ src/manager/ManagerOrdinazione.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+
 import connessione.ConnectionPool;
 import model.BeanOrdinazione;
 
@@ -69,6 +71,47 @@ public class ManagerOrdinazione {
 				ps.close();
 				ConnectionPool.releaseConnection(conn);
 				return ordine;
+
+			}
+			catch(Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	public synchronized BeanOrdinazione ottieniOrdinazione(Integer numeroTavolo) {
+		
+		Connection conn =  null;
+		PreparedStatement ps = null;
+		ManagerTavolo tavoloManager = new ManagerTavolo();
+		Integer numeroOrdine = 0;
+		Timestamp dataOrdine;
+		
+		try {
+			conn = ConnectionPool.getConnection();
+			
+			numeroOrdine = tavoloManager.getOrdinazioneDiTavolo(numeroTavolo);
+			
+			String sqlString = new String("SELECT * FROM Ordinazione WHERE numeroOrdine = ?");
+			ps = conn.prepareStatement(sqlString);
+
+			ps.setInt(1, numeroOrdine);
+			
+			ResultSet res = ps.executeQuery();
+
+			if(res.next()) {
+				dataOrdine = res.getTimestamp("dataOrdine");
+				BeanOrdinazione ordine = new BeanOrdinazione(numeroOrdine, dataOrdine);
+				return ordine;
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				ps.close();
+				ConnectionPool.releaseConnection(conn);
 
 			}
 			catch(Exception e2) {
