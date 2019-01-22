5fab92fec76565ce223d321af597da899aa2cc25
Luca Amoriello <luca.amoriello@hotmail.it>
2019-01-22 15:25:06 +0100

    JUnit 5 dependencies addedd, web.xml configurated (prev commit)

---
+++ src/test/TestManagerUtente.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestManagerUtente {
+
+	@Test
+	public void test() {
+		System.out.println("Dovrebbe funzionare");
+	}
+
+}
