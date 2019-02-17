aa420418f3adf4afa7452e01c47af4f831304cc0
mrgionsi <g.pasquariello96@gmail.com>
2019-02-17 13:50:19 +0100

    print order

--- WebContent/contents/scripts/ResocontoTavolo.js
+++ WebContent/contents/scripts/ResocontoTavolo.js
@@ -1,7 +1,48 @@
+
 $(document).ready(function(){
 	$.get("ServletOttieniOrdinazione",{
-		"numeroTavolo": 1
+		"numeroTavolo": localStorage.getItem("tavoloordinazione")
 	}).done(function(data,status){
 		console.log(JSON.parse(data));
+		var  ordinazione = JSON.parse(data);
+		 setValue(ordinazione);
+		 setProducts(ordinazione.listaPiatti);
 	})
+	
+	$("#ntavolo").text(localStorage.getItem("tavoloordinazione"));
 });
+
+
+
+function setValue(ordinazione){
+	console.log(ordinazione.dataOrdine);
+	$("#dataOrdine").text(ordinazione.dataOrdine);
+	$("#tavoloOrdine").text(localStorage.getItem("tavoloordinazione"));
+	$("#numeroOrdinazione").text(ordinazione.numeroOrdinazione);
+}
+var totale = 0;
+function setProducts(products){
+	var body =$("#table-ordering").children("tbody");
+	products.forEach(function(p){
+		var tr = document.createElement("tr");
+		
+		var td = document.createElement("td");
+		$(td).text(p.nomePiatto);
+		$(tr).append(td);
+		var td = document.createElement("td");
+		$(td).text(p.quantita);
+		$(td).addClass("text-center");
+		$(tr).append(td);
+		var td = document.createElement("td");
+		$(td).text(p.prezzoPiatto + " \u20AC");
+		$(td).addClass("text-center");
+		$(tr).append(td);
+		var td = document.createElement("td");
+		$(td).text(p.quantita * p.prezzoPiatto + " \u20AC");
+		$(td).addClass("text-center");
+		$(tr).append(td);
+		totale = totale + (p.quantita * p.prezzoPiatto);
+		$(body).append(tr);
+	})
+	$("#totaleConto").text(totale + " \u20AC");
+}
